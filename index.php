817e8d13ed732eb2ba6a08aca8b2c72c6b01aac5
KYSU88 <klos.krzysztof88@gmail.com>
2018-09-02 22:56:54 +0200

    Create a login form

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <title>Gra przeglądarkowa</title>
+</head>
 
-echo "helo";
+<body>
+    Gra Online<br /><br />
+    <form action="Login.php" method="post">
+        Login: <br /> <input type="text" name="login"/> <br />
+        Hasło: <br /> <input type="password" name="password"/> <br />
+        <input type="submit" value="Zaloguj się"/>
+    </form>
+</body>
+</html>
