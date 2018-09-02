f804ac6a26bf241dd1e21d630ffa4c603b9e488c
KYSU88 <klos.krzysztof88@gmail.com>
2018-09-03 00:30:03 +0200

    Add getting data from db

--- Login.php
+++ Login.php
@@ -1,18 +1,40 @@
 <?php
-
+    session_start();
     require_once "Connect.php";
 
     $connection = @new mysqli($host, $db_user, $db_password, $db_name);
 
     if($connection->connect_errno!=0)
     {
-      echo "Error: ".$connection->connect_errno.;
+      echo "Error: ".$connection->connect_errno;
     }
     else
     {
         $login = $_POST['login'];
         $password = $_POST['password'];
 
+        $sql = "SELECT * FROM uzytkownicy WHERE user='$login' AND pass='$password'";
+
+        if($result = @$connection->query($sql))
+        {
+            $howManyUsers = $result->num_rows;
+            if($howManyUsers > 0)
+            {
+                $row = $result->fetch_assoc();
+                $_SESSION['user'] = $row['user'];
+                $_SESSION['drewno'] = $row['drewno'];
+                $_SESSION['kamien'] = $row['kamien'];
+                $_SESSION['zboze'] = $row['zboze'];
+                $_SESSION['email'] = $row['email'];
+                $_SESSION['dnipremium'] = $row['dnipremium'];
+
+                $result->free_result();
+                header('Location: Game.php');
+            }else
+            {
+
+            }
+        }
 
         $connection->close();
     }
