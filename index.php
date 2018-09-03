e686a614e5acf07db0178705ba0650671adb0820
KYSU88 <klos.krzysztof88@gmail.com>
2018-09-03 23:08:33 +0200

    Add instruction if user ale logged in then change site to Game.

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
     session_start();
+    if(isset($_SESSION['loggedIn']) && ($_SESSION['loggedIn']==true));
+    {
+        header('Location: Game.php');
+        exit;
+    }
 ?>
 <!DOCTYPE HTML>
 <html lang="pl">
