3df01678371371fbfd2f05ecd9d79591cef6b6b8
KYSU88 <klos.krzysztof88@gmail.com>
2018-09-04 07:16:57 +0200

    Add sql injection

--- Login.php
+++ Login.php
@@ -17,9 +17,12 @@
         $login = $_POST['login'];
         $password = $_POST['password'];
 
-        $sql = "SELECT * FROM uzytkownicy WHERE user='$login' AND pass='$password'";
+        $login = htmlentities($login, ENT_QUOTES, "UTF-8");
+        $password  = htmlentities($password, ENT_QUOTES, "UTF-8");
 
-        if($result = @$connection->query($sql))
+
+        if($result = @$connection->query(sprintf("SELECT * FROM uzytkownicy WHERE user='%s' AND pass='%s'", mysqli_real_escape_string($connection,$login),
+            mysqli_real_escape_string($connection,$password))));
         {
             $howManyUsers = $result->num_rows;
             if($howManyUsers > 0)
