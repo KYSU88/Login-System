02c5ad11e8572adc2f4ce8fb42412063632ab706
KYSU88 <klos.krzysztof88@gmail.com>
2018-09-05 19:02:35 +0200

    Add page structure

---
+++ Registration.php
@@ -0,0 +1,15 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <title>Gra przeglądarkowa - załóż konto</title>
+</head>
+
+<body>
+
+</body>
+</html>
