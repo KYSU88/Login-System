b827790cbeb61a681ee0756f2e7c23da1322634c
KYSU88 <klos.krzysztof88@gmail.com>
2018-09-09 19:35:42 +0200

    Add user in db validation

--- Registration.php
+++ Registration.php
@@ -68,6 +68,37 @@
             $_SESSION['error_captcha'] = "Potwierdź, że nie jesteś botem";
         }
 
+        require_once "Connect.php";
+
+        mysqli_report(MYSQLI_REPORT_STRICT);
+
+        try
+        {
+            $connect = new mysqli($host, $db_user, $db_password, $db_name);
+            if($connect->connect_errno!=0)
+            {
+                throw new Exception(mysqli_connect_errno());
+            }
+            else
+            {
+                $result = $connect->query("SELECT id FROM uzytkownicy WHERE eemail='$email'");
+                if(!$result) throw new Exception($connect->error);
+
+                $how_many_mail = $result->num_rows;
+                if($how_many_mail>0)
+                {
+                    $correctValidation = false;
+                    $_SESSION['error_email'] = "Istnieje juz konto przypisane do tego adresu e-mail";
+                }
+
+                $connect->close();
+            }
+        }catch (Exception $e)
+        {
+            echo '<span style="color:red">Błąd serwera</span>';
+            echo '<br /> Informacja developerska: '.$e;
+        }
+
         if($correctValidation==true)
         {
             // All validation is correct, add user to db
