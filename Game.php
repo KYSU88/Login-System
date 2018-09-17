15dc9e7b53237f8c2b33b5010d1bc82905d25d09
KYSU88 <klos.krzysztof88@gmail.com>
2018-09-17 17:31:07 +0200

    Add calculation of the remaining premium time

--- Game.php
+++ Game.php
@@ -12,7 +12,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
     <title>Gra przeglądarkowa</title>
 </head>
-
 <body>
 <?php
     echo "<p>Witaj ".$_SESSION['user'].'![<a href="LogOut.php">Wyloguj się</a>]</p>';
@@ -20,7 +19,23 @@
     echo " | <b>Kamień</b>: ".$_SESSION['kamien'];
     echo " | <b>Zboże</b>: ".$_SESSION['zboze']."</p>";
     echo "<p><b>E-mail</b>: ".$_SESSION['email'];
-    echo "<br /><b>Dni premium</b>: ".$_SESSION['dnipremium'];
+    echo "<br /><b>Data wygaśnięcia premium</b>: ".$_SESSION['dnipremium']."</p>";
+
+    $ServerDateTime = new DateTime();
+
+    echo "Data i czas serwera: ".$ServerDateTime->format('Y-m-d H:i:s')."<br>";
+
+    $endPremiumTime = DateTime::createFromFormat('Y-m-d H:i:s', $_SESSION['dnipremium']);
+
+    $differenceServerPremiumTime = $ServerDateTime->diff($endPremiumTime);
+    if($ServerDateTime<$endPremiumTime){
+        echo "Pozostało premium: ".$differenceServerPremiumTime->format('%y lat, %m mies, %d dni, %h godz, %i min, %s sek');
+    } else{
+        echo "Premium nieaktywne od: ".$differenceServerPremiumTime->format('%y lat, %m mies, %d dni, %h godz, %i min, %s sek');
+    }
+
+
+
 ?>
 </body>
 </html>
